744958e5e32a7a9a2695fbe5a886d224290c1b5a
Armin Preiml <apreiml@strohwolke.at>
2018-01-17 10:13:44 +0100

    some more edge cases

--- tests/resources/lang/de/special.php
+++ tests/resources/lang/de/special.php
@@ -3,6 +3,7 @@
 return [
     'youare' => 'du bist',
     'special-chars' => 'Das "Anführungszeichen", in \'zwei\' Varianten',
+    'special-chars-v2' => "Das \"doppelte\" Anführungszeichen gibt's auch 'einfach'",
 
     'empty-array' => [],
 ];

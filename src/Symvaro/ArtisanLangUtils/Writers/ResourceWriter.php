fa70b76312fdf5eea94fa2ac7b99edc0380e5552
Armin Preiml <apreiml@strohwolke.at>
2018-01-17 10:06:18 +0100

    simplify resource writer

--- src/Symvaro/ArtisanLangUtils/Writers/ResourceWriter.php
+++ src/Symvaro/ArtisanLangUtils/Writers/ResourceWriter.php
@@ -35,8 +35,7 @@ class ResourceWriter extends Writer
 
             if ($currentFile === null) {
                 $currentFile = $filename;
-            }
-            else if ($currentFile !== $filename) {
+            } else if ($currentFile !== $filename) {
                 $this->writeEntries($currentFile, $currentFileEntries);
                 $currentFile = $filename;
                 $currentFileEntries = [];
@@ -63,8 +62,6 @@ class ResourceWriter extends Writer
         return substr($key, strpos($key, '.') + 1);
     }
 
-    private $currentLevel;
-
     private function writeEntries($filePath, array $entries)
     {
         $filePath = $this->uri . '/' . $filePath;
@@ -85,56 +82,12 @@ class ResourceWriter extends Writer
 
         fwrite($f, "<?php\n\nreturn [\n");
 
-        $this->currentLevel = [];
-
         foreach ($entries as $key => $value) {
-            $level = explode('.', $key);
-
-            for ($i = 0; $i < sizeof($level) - 1 && $i < sizeof($this->currentLevel); $i += 1) {
-                if ($level[$i] != $this->currentLevel[$i]) {
-                    break;
-                }
-            }
-
-            if ($i < sizeof($this->currentLevel)) {
-                $this->closeLevels($f, sizeof($this->currentLevel) - $i);
-            }
-
-            for (; $i < sizeof($level) - 1; $i += 1) {
-                $this->intend($f, sizeof($this->currentLevel) + $i + 1);
-                fwrite($f, "'{$level[$i]}' => [\n");
-            }
-
-            $this->currentLevel = $level;
-            unset($this->currentLevel[sizeof($this->currentLevel) -1]);
-
             $value = str_replace('\'', '\\\'', $value);
 
-            $this->intend($f, sizeof($this->currentLevel) + 1);
-            fwrite($f, "'{$level[sizeof($level)-1]}' => '$value',\n");
+            fwrite($f, "    '$key' => '$value',\n");
         }
-
-        $this->closeLevels($f, sizeof($this->currentLevel));
 
         fwrite($f, "];\n");
-    }
-
-    private function closeLevels($handle, $count)
-    {
-        for ($i = 0; $i < $count; $i += 1) {
-            $this->intend($handle, sizeof($this->currentLevel) - $i);
-            fwrite($handle, "],\n");
-        }
-    }
-
-    private function intend($handle, $level)
-    {
-        $str = '';
-
-        for ($i = 0; $i<$level; $i+=1) {
-            $str .= '    ';
-        }
-
-        fwrite($handle, $str);
     }
 }

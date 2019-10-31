a3aa77282ffcb19d05a2aff6bf508eb1286a8822
Armin Preiml <apreiml@strohwolke.at>
2019-10-31 17:02:21 +0100

    make sure to disable output on require lang files

--- src/Symvaro/ArtisanLangUtils/Readers/ResourceFileReader.php
+++ src/Symvaro/ArtisanLangUtils/Readers/ResourceFileReader.php
@@ -18,10 +18,20 @@ class ResourceFileReader extends Reader
     public function open($uri)
     {
         $this->filesystem = new Filesystem();
+        $this->entries = $this->fetchEntries($uri);
+    }
 
+    static private function fetchEntries($uri)
+    {
+        // ob_start and the clean afterwards makes sure, that there won't be any output upon require.
+        // Otherwise this can result in unwanted output, when e.g. something is written before the
+        // <?php tag. I happened to have a lang file, which had an empty line before the php tag. Which
+        // broke the PO format in the end.
+        ob_start();
         $fileContents = require $uri;
+        ob_clean();
 
-        $this->entries = Collection::make(Arr::dot($fileContents))->getIterator();
+        return Collection::make(Arr::dot($fileContents))->getIterator();
     }
 
     /**
